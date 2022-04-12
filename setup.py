c68ec383e67a2308cde9b20fc45a26669487dc61
DaniloTomassoni <daniloaugustotomassoni@hotmail.com>
2022-04-12 20:38:50 -0300

    upgrade version 0.0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name = 'Text Convert Unicode',
-    version = '1.0.0',
+    name = 'text_convert_unicode',
+    version = '0.0.2',
     author = 'Danilo Tomassoni',
     author_email = 'danilothomassoni@gmail.com',
     packages = ['text_convert_unicode'],
-    description='Text Convert unicode',
+    description='text_convert_unicode',
     long_description='Function tcu ' + 'show details' + 'show reverse details',
     license='MIT',
     platforms=[
